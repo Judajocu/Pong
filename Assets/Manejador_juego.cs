54cf48149874ab6f9e4d550abe978a064294547e
Judajocu <Juandanieljoa@gmail.com>
2016-09-25 16:59:10 -0400

    "Score no entra uno de los if y solo aumenta el score del jugador 2"

--- Assets/Manejador_juego.cs
+++ Assets/Manejador_juego.cs
@@ -9,21 +9,23 @@ public class Manejador_juego : MonoBehaviour {
 
     public GUISkin show;
 
-	public static void Score (string pared) {
-
-        if(pared == "tope_derecho")
+	public static void Score (string pared)
+    {
+        if(pared.Equals("tope_derecho"))
         {
             puntos_jugador_1 += 1;
+            Debug.Log("entro para 1");
         }
         else
         {
+            Debug.Log("entro para 2");
             puntos_jugador_2 += 1;
         }
     }
     void OnGUI ()
     {
         GUI.skin = show;
-        GUI.Label(new Rect(Screen.width / 2 - 150,  30, 100, 100), "" + puntos_jugador_1);
-        GUI.Label(new Rect(Screen.width / 2 + 150,  30, 100, 100), "" + puntos_jugador_2);
+        GUI.Label(new Rect(Screen.width / 2 - 100,  30, 100, 100), "" + puntos_jugador_1);
+        GUI.Label(new Rect(Screen.width / 2 + 100,  30, 100, 100), "" + puntos_jugador_2);
     }
 }
