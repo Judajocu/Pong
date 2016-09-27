2a8aebb6afcf5d0ca7ca3ece680cedb270879f0d
Judajocu <Juandanieljoa@gmail.com>
2016-09-26 23:44:08 -0400

    "Reset de la bola arreglado"

--- Assets/Control_bola.cs
+++ Assets/Control_bola.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Control_bola : MonoBehaviour {
+public class Control_bola : MonoBehaviour
+{
 
     // Use this for initialization
     public Rigidbody2D rb;
     float ballspeed = 100;
     public float velocity_y;
     public float velocity_x;
+
     void Start ()
     {
         Invoke("GoBall", 2.0f); 
 	}
 
     // Update is called once per frame
-    void OnCollision2D(Collision2D colInfo) {
+    void OnCollision2D(Collision2D colInfo)
+    {
         if(colInfo.collider.tag == "Jugador")
         {
             velocity_y = GetComponent<Rigidbody2D>().velocity.y/2 + colInfo.collider.GetComponent<Rigidbody2D>().velocity.y/3;
@@ -23,11 +26,13 @@ public class Control_bola : MonoBehaviour {
 
     void ResetBall()
     {
-        velocity_x = GetComponent<Rigidbody2D>().velocity.x * 0;
-        velocity_y = GetComponent<Rigidbody2D>().velocity.y * 0;
-        gameObject.transform.Translate(0, 0, 0);
+        rb = GetComponent<Rigidbody2D>();
 
-        Invoke("GoBall", 0.5f);
+        rb.velocity = new Vector3(0, 0);
+        gameObject.transform.Translate(0, 0, 0);
+        gameObject.transform.position = new Vector3(0,0,0);
+
+        Invoke("GoBall", 1.0f);
 
     }
 
